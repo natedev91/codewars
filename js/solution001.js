b0ed7b5aee98343da519e977db12c899bcfc8f8a
Nate <81495717+kickinwing0991@users.noreply.github.com>
2022-05-09 12:52:56 +0000

    completed codewars repo setup

---
+++ js/solution001.js
@@ -0,0 +1,25 @@
+/* Given an array of integers.
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+0 is neither positive nor negative.
+If the input is an empty array or is null, return an empty array.
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65]. */
+
+
+function countPositivesSumNegatives(input) {
+  if (input === null || input.length === 0) {
+    return [];
+  }
+  let posCount = 0;
+  let negSum = 0;
+  for (const element of input) {
+     if (element > 0) {
+      posCount++;
+    } else {
+      negSum += element;
+    }
+  }
+  return [posCount, negSum];
+}
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
